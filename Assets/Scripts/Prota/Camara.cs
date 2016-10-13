45f6e6db4135d5e03d2168e3d2bc91d10c8b8513
morathias <matias_morales_1996@hotmail.com>
2016-10-13 13:07:27 -0300

    Movimiento Player
    *se implemento movimiento basico
    *se implemento un camara follow
    *arreglar que se ponga arriba del player

---
+++ Assets/Scripts/Prota/Camara.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour {
+    public Transform target;
+    public float suavizado;
+    public float altura;
+    public float distancia;
+
+	void Update () {
+        if (!target)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * suavizado);
+        transform.position = new Vector3(transform.position.x, altura, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Prota/Prota.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+//==================================================================
+public class Prota : MonoBehaviour {
+    public float velocidad = 5f;
+    
+    Vector3 _direccion = Vector3.zero;
+    CharacterController _protaController;
+    //--------------------------------------------------------------
+	void Start () {
+        _protaController = GetComponent<CharacterController>();
+	}
+    //--------------------------------------------------------------
+	void Update () {
+        _direccion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _direccion = transform.TransformDirection(_direccion);
+        _direccion *= velocidad;
+
+        _protaController.Move(_direccion * Time.deltaTime);
+	}
+    //--------------------------------------------------------------
+}
