dd7fdbdd491634bddc809c153a8a0d955938bfcc
morathias <matias_morales_1996@hotmail.com>
2016-10-17 12:59:44 -0300

    Stamina-Vida
    *se implemento barra de vida y stamina
    *se arreglo el dash

--- Assets/Scripts/Prota/Prota.cs
+++ Assets/Scripts/Prota/Prota.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 //===============================================================================================
 public class Prota : MonoBehaviour {
     public float velocidad = 5f;
     float _angulo;
-    
+    int _stamina = 100;
+
     Vector3 _direccion = Vector3.zero;
     Vector3 _posicionDelMouse;
     Vector3 _posicionDelPlayer;             //en coordenadas de pantalla
     Vector3 _posicionEsquivar;
 
     CharacterController _protaController;
+
+    Stats _stats;
+
+    public Image _barraVida;
+    public Image _barraStamina;
 
     enum estados {      //para la maquina de estados
         moviendose,
@@ -20,19 +27,21 @@ public class Prota : MonoBehaviour {
     }
     estados _estado = estados.moviendose;
 
-    float _timerEsquivar = 0.5f;
+    float _timerEsquivar = 0.2f;
     //-------------------------------------------------------------------------------------------
 	void Start () {
+        _stats = GetComponent<Stats>();
         _protaController = GetComponent<CharacterController>();
 	}
     //-------------------------------------------------------------------------------------------
 	void Update () {
-        switch (_estado) { 
+        switch (_estado) {
             case estados.moviendose:
                 moverProta();
                 rotarProta();
 
-                if (Input.GetKeyDown(KeyCode.Space)){
+                if (Input.GetKeyDown(KeyCode.Space) && _stamina > 0){
+                    perderStamina();
                     _posicionEsquivar = new Vector3((_direccion.x * 1.2f) + transform.position.x,
                                                     transform.position.y, 
                                                     (_direccion.z * 1.2f) + transform.position.z);
@@ -42,11 +51,13 @@ public class Prota : MonoBehaviour {
 
             case estados.esquivando:
                 esquivar();
-
+                perderVidaTest();
+                
                 _timerEsquivar -= Time.deltaTime;
-                if (_timerEsquivar <= 0){
+                if (_timerEsquivar <= 0)
+                {
                     _estado = estados.moviendose;
-                    _timerEsquivar = 0.5f;
+                    _timerEsquivar = 0.2f;
                 }
                 break;
 
@@ -60,13 +71,11 @@ public class Prota : MonoBehaviour {
         _direccion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _direccion *= velocidad;
 
-        //_protaController.Move(_direccion * Time.deltaTime);
         _protaController.SimpleMove(_direccion);
     }
     //-------------------------------------------------------------------------------------------
     void esquivar() {
-        transform.position = Vector3.LerpUnclamped(transform.position, _posicionEsquivar, 
-                                                   Time.deltaTime * velocidad);
+        _protaController.SimpleMove(_direccion * 3f);
     }
     //-------------------------------------------------------------------------------------------
     void rotarProta() {
@@ -81,8 +90,19 @@ public class Prota : MonoBehaviour {
         _angulo = Mathf.Atan2(_posicionDelMouse.x, _posicionDelMouse.y) * Mathf.Rad2Deg;    //saco el angulo de ese vector
         transform.rotation = Quaternion.Euler(new Vector3(0, _angulo, 0));      //lo roto en eje y
     }
+
     public void stunE() {
         _estado = estados.explosion;
+    }
+
+    void perderVidaTest() {
+        _stats.health -= 1;
+        _barraVida.fillAmount = _stats.health / 100f;
+    }
+
+    void perderStamina() {
+        _stamina -= 10;
+        _barraStamina.fillAmount = _stamina / 100f;
     }
 }
 //===============================================================================================
