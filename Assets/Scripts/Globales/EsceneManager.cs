957db3c7220cc97e225891a845de8f66b852b3cd
morathias <matias_morales_1996@hotmail.com>
2018-11-04 17:34:16 -0300

    Fade in and out

--- Assets/Scripts/Globales/EsceneManager.cs
+++ Assets/Scripts/Globales/EsceneManager.cs
@@ -1,18 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EsceneManager : MonoBehaviour
 {
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
+    private Image _fadeOverlay;
+    private int _levelToLoad;
+
+    public float fadeTime = 1f;
+
+    void Start() {
+        _fadeOverlay = transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        StartCoroutine(startFadingOut());
     }
 
     public void cambiarEscena(int index)
     {
-        //LoadingScreenManager.LoadScene(index);
-        SceneManager.LoadScene(index);
+        _levelToLoad = index;
+        StartCoroutine(startFadingIn());
+    }
+
+    IEnumerator startFadingOut() {
+        Color color = _fadeOverlay.color;
+
+        float timeStarted = Time.realtimeSinceStartup;
+
+        while (color.a > 0f){
+            float timeSinceStarted = Time.realtimeSinceStartup - timeStarted;
+            float percentage = timeSinceStarted / fadeTime;
+
+            color.a = Mathf.Lerp(1f, 0f, percentage);
+            _fadeOverlay.color = color;
+            yield return null;
+        }
+
+        _fadeOverlay.enabled = false;
+    }
+
+    IEnumerator startFadingIn()
+    {
+        _fadeOverlay.enabled = true;
+        Color color = _fadeOverlay.color;
+
+        float timeStarted = Time.realtimeSinceStartup;
+
+        while (color.a < 1f)
+        {
+            float timeSinceStarted = Time.realtimeSinceStartup - timeStarted;
+            float percentage = timeSinceStarted / fadeTime;
+
+            color.a = Mathf.Lerp(0f, 1f, percentage);
+            _fadeOverlay.color = color;
+            yield return null;
+        }
+
+        onFadeCompleted();
+    }
+
+    private void onFadeCompleted() {
+        SceneManager.LoadScene(_levelToLoad);
         Time.timeScale = 1;
     }
 
